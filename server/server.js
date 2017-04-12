ff9e6e9de54344b5ecdfbc103530f440e997b65c
Cameron <camdunne@gmail.com>
2017-04-12 19:56:07 -0400

    multi carousel attempt

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
+const scrapeIt = require('scrape-it');
 dotenv.config();
 
 const pg = require('./config/database.js');
@@ -21,3 +22,21 @@ const server = app.listen(8888, () => {
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+// SCRAPE-IT EXAMPLE
+// scrapeIt("https://www.viator.com/search/new%20york%20city", {
+// pages: {
+//   listItem: ".media",
+//
+//   data: {
+//     title: "a",
+//     price: ".price-amount",
+//     url: {
+//     selector: "a",
+//     attr: "href"
+//       }
+//     }
+//   }
+// }).then(page => {
+//     console.log(page);
+// });
